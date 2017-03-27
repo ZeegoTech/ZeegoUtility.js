61021993f296ff0a3d6cef41876f2eeff3e77149
RobbyShao <shaoqi@zeego.cn>
2017-03-27 17:00:58 +0800

    解决了windows下不能build的问题

---
+++ build/build.js
@@ -0,0 +1,20 @@
+var ora = require('ora')
+var webpack = require('webpack')
+var webpackConfig = require('../webpack.production.config.js')
+
+process.env.NODE_ENV = 'production'
+
+var spinner = ora('building for production...')
+spinner.start()
+
+webpack(webpackConfig, function (err, stats) {
+  spinner.stop()
+  if (err) throw err
+  process.stdout.write(stats.toString({
+    colors: true,
+    modules: false,
+    children: false,
+    chunks: false,
+    chunkModules: false
+  }) + '\n')
+})
